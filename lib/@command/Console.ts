9f0fb4a7b831fede173847fa0bd7ca5fa3f355b3
tsiresy.mila <tsiresy.mila@ezway-technology.com>
2022-05-09 14:00:38 +0300

    update command and template

--- lib/@command/Console.ts
+++ lib/@command/Console.ts
@@ -5,103 +5,113 @@ const clc = require("cli-color");
 const path = require("path");
 const argv = yargs(hideBin(process.argv)).argv;
 
-export class Console {
+interface ConsoleInterface {
+    handle : () => void
+}
+export class Console implements ConsoleInterface {
 
     public handle() {
         if (argv._.length > 1) {
-            var cmd = argv._[0].toString();
-            var srcname = argv._[1].toString();
-            var template = "template.txt";
-            var cmdPath = cmd.replace(":", "/");
-            var srcPath = "";
-            var filePath = path.join(__dirname, '..', '..', "template", cmdPath, template);
-            switch (cmd.toLowerCase()) {
-                case "controller":
-                    srcPath = path.join(process.cwd(), "src", "controllers", srcname + ".ts");
-                    break;
-                case "middleware":
-                    srcPath = path.join(process.cwd(), "src", "middlewares", srcname + ".ts");
-                    break;
-                case "model":
-                    srcPath = path.join(process.cwd(), "src", "models", srcname + ".ts");
-                    break;
-                case "schema":
-                    srcPath = path.join(process.cwd(), "src", "schema", srcname + ".ts");
-                    break;
-                case "repository":
-                    srcPath = path.join(process.cwd(), "src", "repository", srcname + ".ts");
-                    if(argv.crud){
-                        var filePath = path.join(__dirname, '..', '..', "template", cmdPath, 'crud.txt');
-                    }
-                    break;
-                case "socket":
-                case "socket:controller":
-                    srcPath = path.join(process.cwd(), "src", "sockets", srcname + ".ts");
-                    break;
-                case "socket:middleware":
-                    srcPath = path.join(process.cwd(), "src", "sockets", srcname + ".ts");
-                    break;
-                case "view":
-                    var ext = "html.twig";
-                    srcPath = path.join(process.cwd(), "src", "views", srcname + "." + ext);
-                    if (argv.folder) {
-                        var viewFolder = argv.folder;
-                        srcPath = path.join(process.cwd(), "src", "views", viewFolder);
-                        if (!fs.existsSync(srcPath)) {
-                            fs.mkdirSync(srcPath);
+            try{
+                var cmd = argv._[0].toString();
+                var srcname = argv._[1].toString();
+                var template = "template.txt";
+                var cmdPath = cmd.replace(":", "/");
+                var srcPath = "";
+                var filePath = path.join(__dirname, '..', '..', "template", cmdPath, template);
+                switch (cmd.toLowerCase()) {
+                    case "controller":
+                        srcPath = path.join(process.cwd(), "src", "controllers", srcname + ".ts");
+                        break;
+                    case "middleware":
+                        srcPath = path.join(process.cwd(), "src", "middlewares", srcname + ".ts");
+                        break;
+                    case "model":
+                        srcPath = path.join(process.cwd(), "src", "models", srcname + "Model.ts");
+                        break;
+                    case "schema":
+                        srcPath = path.join(process.cwd(), "src", "schema", srcname + ".ts");
+                        break;
+                    case "repository":
+                        srcPath = path.join(process.cwd(), "src", "repository", srcname + "Repository.ts");
+                        if(argv.crud){
+                            var filePath = path.join(__dirname, '..', '..', "template", cmdPath, 'crud.txt');
                         }
-                        srcPath = path.join(srcPath, srcname + "." + ext);
-                    }
-                    if (argv.ext) {
-                        ext = argv.ext;
-                    }
-                    var viewPath = path.join(
-                        process.cwd(),
-                        "node_modules",
-                        "easy-ts-api",
-                        "template",
-                        "view",
-                        "index." + ext
-                    );
-                    if (fs.existsSync(viewPath)) {
-                        var content = fs.readFileSync(viewPath);
-                        fs.writeFileSync(srcPath, content.toString());
-                        console.log(
-                            clc.white.bgGreen(`${cmd.replace(":", " ")} created successfully`)
+                        break;
+                    case "socket":
+                    case "socket:controller":
+                        srcPath = path.join(process.cwd(), "src", "sockets", srcname + ".ts");
+                        break;
+                    case "socket:middleware":
+                        srcPath = path.join(process.cwd(), "src", "sockets", srcname + ".ts");
+                        break;
+                    case "view":
+                        var ext = "html.twig";
+                        srcPath = path.join(process.cwd(), "src", "views", srcname + "." + ext);
+                        if (argv.folder) {
+                            var viewFolder = argv.folder;
+                            srcPath = path.join(process.cwd(), "src", "views", viewFolder);
+                            if (!fs.existsSync(srcPath)) {
+                                fs.mkdirSync(srcPath);
+                            }
+                            srcPath = path.join(srcPath, srcname + "." + ext);
+                        }
+                        if (argv.ext) {
+                            ext = argv.ext;
+                        }
+                        var viewPath = path.join(
+                            process.cwd(),
+                            "node_modules",
+                            "easy-ts-api",
+                            "template",
+                            "view",
+                            "index." + ext
                         );
-                    }
-                    break;
-                default:
-                    this.showHelp();
-                    break;
+                        if (fs.existsSync(viewPath)) {
+                            var content = fs.readFileSync(viewPath);
+                            fs.writeFileSync(srcPath, content.toString());
+                            cmd = cmd[0].toUpperCase() + cmd.slice(1)
+                            console.log(
+                                clc.green(`✅ ${cmd.replace(":", " ")} ${srcname} created successfully.`)
+                            );
+                        }
+                        break;
+                    default:
+                        this.showHelp();
+                        break;
+                }
+                if (fs.existsSync(filePath)) {
+                    var content = fs.readFileSync(filePath);
+                    content = content.toString().replaceAll("{{name}}", srcname);
+                    console.log(srcPath);
+                    fs.writeFileSync(srcPath, content);
+                    cmd = cmd[0].toUpperCase() + cmd.slice(1)
+                    console.log(
+                        clc.green(`✅ ${cmd.replace(":", " ")} ${srcname} created successfully.`)
+                    );
+                }
             }
-            if (fs.existsSync(filePath)) {
-                var content = fs.readFileSync(filePath);
-                content = content.toString().replaceAll("{{name}}", srcname);
-                console.log(srcPath);
-                fs.writeFileSync(srcPath, content);
-                console.log(
-                    clc.white.bgGreen(`${cmd.replace(":", " ")} ${srcname} created successfully`)
-                );
+            catch(e : any){
+                clc.clc.white.bgRed(`❌ ${e.toString()}`)
             }
         } else {
             if (argv.helpme) {
                 this.showHelp();
             } else {
                 console.log(
-                    clc.white.bgRed("Unknow command, use --helpme to show avalaible command.")
+                    clc.white.bgRed("❌ Unknow command, use --helpme to show avalaible command.")
                 );
             }
         }
     }
     public showHelp() {
         console.log("List of avalaible command: ");
-        console.log("\tcontroller <name> -- to create controller");
-        console.log("\tmiddleware <name> -- to create middleware");
-        console.log("\tmodel <name> -- to create model");
-        console.log("\trepository <model name> -- to create repository");
-        console.log("\tsocket <name> -- to create controller");
-        console.log("\tsocket:middleware <name> -- to create controller");
-        console.log("\tview <name> --folder=<folder> --ext=<extension>  -- to create view");
+        console.log("\t• controller <name> -- to create controller");
+        console.log("\t• middleware <name> -- to create middleware");
+        console.log("\t• model <name> -- to create model");
+        console.log("\t• repository <model name> -- to create repository");
+        console.log("\t• socket <name> -- to create controller");
+        console.log("\t• socket:middleware <name> -- to create controller");
+        console.log("\t• view <name> --folder=<folder> --ext=<extension>  -- to create view");
     }
 }
--- lib/@decorators/Params.ts
+++ lib/@decorators/Params.ts
@@ -1,6 +1,6 @@
 import { ParamsKey } from "../@types";
 
-const paramsFactory = (ptype: string)=> {
+const paramsDecorator = (ptype: string)=> {
     return (key?: string) => {
         return (target: any, propertyKey: string, parameterIndex: number) => {
             var t = Reflect.getMetadata("design:type", target, propertyKey.toString());
@@ -27,16 +27,16 @@ const paramsFactory = (ptype: string)=> {
     
 }
 
-export const Req = paramsFactory('req')
-export const Res = paramsFactory('res')
-export const Params = paramsFactory('params')
-export const Body = paramsFactory('body')
-export const Query = paramsFactory('query')
-export const Headers = paramsFactory('headers')
-export const Files = paramsFactory('files')
-export const Session = paramsFactory('session')
-export const Cookies = paramsFactory('cookies')
-export const Ip = paramsFactory('ip')
-export const Database = paramsFactory('db')
-export const SocketIO = paramsFactory('io')
-export const View = paramsFactory('renderer')
+export const Req = paramsDecorator('req')
+export const Res = paramsDecorator('res')
+export const Params = paramsDecorator('params')
+export const Body = paramsDecorator('body')
+export const Query = paramsDecorator('query')
+export const Headers = paramsDecorator('headers')
+export const Files = paramsDecorator('files')
+export const Session = paramsDecorator('session')
+export const Cookies = paramsDecorator('cookies')
+export const Ip = paramsDecorator('ip')
+export const Database = paramsDecorator('db')
+export const SocketIO = paramsDecorator('io')
+export const View = paramsDecorator('renderer')
